130b378d4db5ae65a3acae4a10049a5c5bcbe705
coline.van-leeuwen@flutilliant.com <coline.van-leeuwen@flutilliant.com>
2021-07-22 12:36:56 +0200

    Added a form to enter the author's name

--- frontend/src/App/SearchAnAuthor.jsx
+++ frontend/src/App/SearchAnAuthor.jsx
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
-import { Container, Segment } from 'semantic-ui-react';
+import {
+  Container, Form, Segment,
+} from 'semantic-ui-react';
 
 class SearchAnAuthor extends Component {
-  render() {
-    return (
-      <Container>
-        <Segment>Work in progress</Segment>
-      </Container>
-
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+    };
   }
+
+    handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+    handleSubmitSearchAuthor = () => {
+      const { name } = this.state;
+      console.log(name);
+    }
+
+    render() {
+      const { name } = this.state;
+
+      return (
+        <Container>
+          <Form onSubmit={this.handleSubmitSearchAuthor}>
+            <Form.Group>
+              <Form.Input
+                inline
+                placeholder={"Nom de l'auteur"}
+                name="name"
+                value={name}
+                onChange={this.handleChange}
+                label={"Nom de l'auteur"}
+              />
+              <Form.Button type="submit">Chercher</Form.Button>
+            </Form.Group>
+          </Form>
+
+          <Segment>Work in progress</Segment>
+        </Container>
+
+      );
+    }
 }
 
 export { SearchAnAuthor };
